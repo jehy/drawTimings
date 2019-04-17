8b8873a79836c34869e45515068d910a2010b152
Jehy <fate@jehy.ru>
2019-04-17 16:25:44 +0300

    draft

---
+++ src/js/index.js
@@ -0,0 +1,126 @@
+'use strict';
+
+import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootswatch/slate/bootstrap.css'; //uncomment to use dark theme
+import '../css/custom.css';
+
+import 'bootstrap';
+
+import * as moment from 'moment';
+import * as Highcharts from 'highcharts';
+import * as boost from 'highcharts/modules/boost';
+import * as HighchartsMore from 'highcharts/highcharts-more';
+import * as dataModule from 'highcharts/modules/data';
+import * as sunburst from 'highcharts/modules/sunburst';
+
+boost(Highcharts);
+dataModule(Highcharts);
+HighchartsMore(Highcharts);
+sunburst(Highcharts);
+
+function addTimeLineGraph(graph, data)
+{
+  let maxTiming = 0;
+  let minTiming = null;
+  const simple = data.timings.reduce((res, timing) => {
+    if (!timing.name)
+    {
+      return res;
+    }
+    if (timing.name.includes(' END '))
+    {
+      return res;
+    }
+    const name = timing.name.replace(' END', '').replace(' START', '').trim();
+    const start = timing.time;
+    if (minTiming === null || minTiming > start)
+    {
+      minTiming = start;
+    }
+    const endTiming = data.timings
+      .find(timingSearch=>timingSearch.name && timingSearch.name.includes(' END')
+        && timingSearch.name.replace(' END', ' START') === timing.name);
+    let end;
+    if (endTiming)
+    {
+      end = endTiming.time;
+      if (maxTiming < end)
+      {
+        maxTiming = end;
+      }
+    }
+    const obj = {name, start, end};
+    res.push(obj);
+    return res;
+  }, []);
+  simple.forEach((el)=>{
+    if (!el.end)
+    {
+      el.end = maxTiming;
+    }
+    el.start -= minTiming;
+    el.start /= 1000;
+    el.end -= minTiming;
+    el.end /= 1000;
+  });
+  const dataColumns = simple.map(data2 => data2.name);
+  const valueColumns = simple.map(data2 => ([data2.start, data2.end]));
+  const options = {
+    chart: {
+      type: 'columnrange',
+      inverted: true,
+      zoomType: 'x',
+      renderTo: graph[0],
+      events: {
+        load: (chart) => {
+          setTimeout(() => chart.target.reflow(), 1000);
+        },
+      },
+    },
+    title: {
+      text: 'search speed',
+    },
+    subtitle: {
+      // eslint-disable-next-line no-undef
+      text: document.ontouchstart === undefined
+        ? 'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in',
+    },
+    xAxis: {
+      categories: dataColumns,
+    },
+    yAxis: {
+      title: {
+        text: 'timing',
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+    series: [{
+      name: 'Timing',
+      data: valueColumns,
+    }],
+  };
+  Highcharts.chart(options);
+}
+
+
+$('#show').click(() => {
+  $('#graphs').empty();
+  $('textarea[name=timing]').each(function () {
+    const graph = $('<div/>');
+    const textarea = $(this);
+    const val = $(textarea).val().trim();
+    if (!val)
+    {
+      return;
+    }
+    const data = JSON.parse(val);
+    addTimeLineGraph(graph, data);
+    $('#graphs').append(graph);
+  });
+});
+
+$('#addTextarea').click(()=>{
+  $('#timings').append('<textarea class="form-control" name="timing" style="height: 100px;"></textarea>');
+});
---
+++ webpack.prod.js
@@ -0,0 +1,84 @@
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  CleanWebpackPlugin = require('clean-webpack-plugin'),
+  webpack = require('webpack'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  devtool: 'source-map',
+  cache: true,
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery' }),
+    new webpack.ContextReplacementPlugin(/moment[\\/]locale$/, /^\.\/(ru|en)$/), // https://github.com/webpack/webpack/issues/87
+    new BundleAnalyzerPlugin({analyzerMode: 'static', openAnalyzer: false}),
+    new ExtractTextPlugin('styles.css'),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: './src/images/favicon.ico',
+      template: './src/index.ejs',
+      inject: true,
+      hash: true,
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    tls: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    console: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader',
+        }),
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
